a4f7f0f6db4fab90942ba63831a03c37b5dbb8c0
larsvanherwijnen <77294984+larsvanherwijnen@users.noreply.github.com>
2021-04-01 16:53:24 +0200

    google login woking, changed to axios, working search function

---
+++ src/Components/firebase.utils.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCVIxqnJEAIPyLWKK1XQhXE4QHASN7HOHM",
+    authDomain: "crypto-app-281d6.firebaseapp.com",
+    projectId: "crypto-app-281d6",
+    storageBucket: "crypto-app-281d6.appspot.com",
+    messagingSenderId: "709601804408",
+    appId: "1:709601804408:web:9af38462aaf8a92b886d8d",
+    measurementId: "G-P2PCEVYWRP"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
